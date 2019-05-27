b5c0ae0f8dec60915b3419d47c34b58a75c2f1f5
yiyo338 <32116409+yiyo338@users.noreply.github.com>
2019-05-27 06:04:15 -0600

    Analizador 60% , Dijkstra.
    Se implementó el poder hablar con el programa.
    Se implementó Dijkstra para calcular la mejor ruta.

--- Logica.pl
+++ Logica.pl
@@ -1,8 +1,41 @@
 :-style_check(-singleton).
-iniciar():-recibir_mensaje([]).
-recibir_mensaje(X):-write('Bienvenido a WazeLog la mejor lógica de llegar a su destino. 
-						   Por Favor indíqueme donde se encuentra.'),
-					write('Usuario: '),read(Entrada), atomic_list_concat(M, ' ', Entrada), oracion(M,Entrada).
+iniciar():-pregunta1().
+
+
+
+o(o(SN,SV)) --> sn(SN,_Gen,Num),sv(SV,Num);sn(SN,_Gen,Num); sv(SV, Num); yn(YN), sn(SN,_Gen,Num).
+
+ 
+sn(sn(DET,N),Gen,Num) --> det(DET,Gen,Num), n(N,Gen,Num); sal(SAL,Num),n(N,Gen,Num); n(N,Gen, Num).
+
+
+sv(sv(VT,SN),Num) --> vt(VT,Num), sn(SN,_Gen,_Num).
+sv(sv(VI),Num) --> vi(VI,Num).
+
+
+det(det(X),f,sg) --> [X], {member(X,[la,una,para])}.
+det(det(X),f,pl) --> [X], {member(X,[las,unas])}.
+det(det(X),m,sg) --> [X], {member(X,[el,un,en,para])}.
+det(det(X),m,pl) --> [X], {member(X,[los,unos])}.
+
+sal(sal(X), sg) --> [X] ,{member(X,[hola,buenosdias])}.
+
+yn(yn(X)) --> [X] ,{member(X,[si,no])}.
+
+vi(vi(ladra),sg) --> [ladra].
+vi(vi(ladran),pl) --> [ladran].
+vi(vi(ladran),sg) --> [estoy].
+vt(vt(estoy),sg) --> [estoy].
+vt(vt(voy),sg) --> [voy].
+vt(vt(muerden),pl) --> [muerden].
+
+
+n(n(perra),f,sg) --> [perra].
+n(n(perras),f,pl) --> [perras].
+n(n(X),m,sg) --> [DrWaze].
+n(n(X),m,sg) --> [X], {lugares(X)}.
+
+n(n(X),m,pl) --> [X], {member(X,[perros,huesos])}.
 
 %...DEFINICION DE LOS LUGARES DEL GRAFO...
 lugares(cartago).
@@ -18,46 +51,110 @@ lugares(orosi).
 lugares(juanvinas).
 lugares(turrialba).
 
-%...REGLA PARA DESTINO Y ORIGEN...
-origen(X):-lugares(X).
-destino(X):-lugares(X).
 
-%...DEFINICION DE LOS VECINOS DE LOS LUGARES EN EL GRAFO...
-vecinos(cartago, [sanjose,tresrios,pacayas,paraiso,musgoverde]).
-vecinos(tresrios, [pacayas]).
-vecinos(sanjose, [cartago,corralillo]).
-vecinos(corralillo, [musgoverde,sanjose]).
-vecinos(musgoverde, [cartago,corralillo]).
-vecinos(paraiso, [cervantes,orosi,cachi]).
-vecinos(pacayas, [tresrios,cartago,cervantes]).
-vecinos(cervantes, [pacayas,juanvinas,cachi]).
-vecinos(cachi, [cervantes,turrialba,orosi,paraiso]).
-vecinos(orosi, [paraiso,cachi]).
-vecinos(juanvinas, [turrialba]).
-vecinos(turrialba, [cachi]).
+%...SERIE DE PREGUNTAS Y RESPUESTAS...
+recibir_mensaje(M):-write('Usuario: '),read(Entrada), atomic_list_concat(M, ' ', Entrada), phrase(o(A),M).
 
+pregunta1():-write('DrWaze: Bienvenido a WazeLog la mejor logica de llegar a su destino. \nPor Favor indiqueme donde se encuentra. \n'),
+			 recibir_mensaje(M), respuesta1(M).
 
-%...DEFINICION DE ORACION...
+respuesta1(M):-lugares(X), miembro(X,M), pregunta2(X); 
+			   write('DrWaze: No puedo entenderle, por favor indiqueme de manera correcta donde se encuentra.\n'), recibir_mensaje(M), respuesta1(M).
 
-oracion(E0,E1):-sintagma_nominal(E0,E0).
+pregunta2(O):-write('DrWaze: Muy bien ¿Cual es su destino?\n'),
+			  recibir_mensaje(M), respuesta2(M,O).
 
+respuesta2(M,O):-lugares(X), miembro(X,M), pregunta3(O,X); 
+			     write('DrWaze: No puedo entenderle, por favor indiqueme de manera correcta su destino.\n'), recibir_mensaje(M), respuesta2(M,O).
 
-%...DEFINICION DE LAS POSIBILIDADES DE ORACION...
-esUnLugar([X|_]):-lugares(X).
+pregunta3(O,D):-write('DrWaze: ¿Tiene algun destino intermedio?\n'),
+			    recibir_mensaje(M), respuesta3(O,D,M).
 
+respuesta3(O,D,M):-miembro(si,M),lugares(X),miembro(X,M),respuestaFinal1(O,D,I);
+				   miembro(si,M),lugares(X), not(miembro(X,M)), preguntarI(I), respuestaFinal1(O,D,I);
+				   miembro(no,M), respuestaFinal2(O,D);
+				   write('DrWaze: No puedo entenderle, intente de nuevo.\n'), pregunta3(O,D).
 
-%...FUNCIONES...
+preguntarI(I):-write('DrWaze: ¿Cual es el destino intermedio?\n'), recibir_mensaje(M), lugares(I), miembro(I,M);
+			  write('DrWaze: No puedo entenderle, intente de nuevo.\n'), preguntarI(I).
+
+respuestaFinal1(O,D,I):-write('DrWaze: Su ruta seria: '), ruta(O,I),ruta(I,D).
+
+respuestaFinal2(O,D):-write('DrWaze: Su ruta seria: '), ruta(O,D).
+
 miembro(X,[X|_]).
 miembro(X,[_|R]):-miembro(X,R).
 
-esVecino(X,Y):-vecinos(X,P), miembro(Y,P).
+:-dynamic
+	rpath/2.
 
-%...DEFINICION DE SUSTANTIVOS...
-sustantivo([drWaze|_], E1).
+arco(sanjose, cartago, 20).
+arco(sanjose,corralillo,22).
+arco(cartago,paraiso,10).
+arco(cartago,pacayas,13).
+arco(cartago,sanjose,20).
+arco(cartago,musgoverde,10).
+arco(cartago,tresrios,8).
+arco(corralillo, sanjose,22).
+arco(corralillo, musgoverde,10).
+arco(paraiso, cervantes,4).
+arco(paraiso, cachi,10).
+arco(paraiso,orosi,8).
+arco(pacayas, tresrios,15).
+arco(pacayas, cartago,13).
+arco(pacayas,cervantes,8).
+arco(musgoverde, corralillo,6).
+arco(musgoverde, cartago,10).
+arco(tresrios, sanjose,8).
+arco(tresrios, pacayas,15).
+arco(cervantes, juanvinas,5).
+arco(cervantes, pacayas,8).
+arco(cervantes, cachi,7).
+arco(cachi,orosi,12).
+arco(cachi, paraiso,10).
+arco(cachi,turrialba,40).
+arco(cachi,cervantes,7).
+arco(orosi, paraiso,8).
+arco(orosi,cachi,12).
+arco(juanvinas, turrialba,4).
+arco(turrialba, pacayas,18).
+arco(paraiso, cervantes,4).
+arco(paraiso, cachi,10).
+arco(paraiso, orosi,8).
 
-%...DEFINICION DE SINTAGMA NOMINAL...
-sintagma_nominal(E0, [_|E1]):- enlace(E0,E1),esUnLugar(E1), write('es nominal'); esUnLugar(E0),write('Ok').
+camino(Ini,Fin,Dist) :- arco(Ini,Fin,Dist).
 
-%...DEFINICION DE ENLACES...
-enlace([En|_],Y).
+
+mascorto([H|Path], Dist) :-
+	rpath([H|T], D), !, Dist < D,
+	retract(rpath([H|_],_)),
+        assert(rpath([H|Path], Dist)).
+mascorto(Path, Dist) :-
+         assert(rpath(Path,Dist)).
+
+
+
+atravesar(Inicio, Camino, Dist) :-
+	camino(Inicio, T, D),
+	not(memberchk(T, Camino)),
+	mascorto([T,Inicio|Camino], Dist+D),
+	atravesar(T,[Inicio|Camino],Dist+D).
+
+atravesar(Inicio) :-
+	retractall(rpath(_,_)),
+	atravesar(Inicio,[],0).
+atravesar(_).
+
+
+
+
+ruta(Inicio, Fin) :-
+	atravesar(Inicio),
+	rpath([Fin|R], Dist)->
+        reverse([Fin|R], Camino),
+        Distancia is round(Dist),
+        writef('Su ruta es %w y recorrerá: %w km \n',
+	       [Camino, Distancia]);
+	writef('No conozco la ruta de %w a %w\n', [Inicio, Fin]).
+
 
