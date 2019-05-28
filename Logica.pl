6c3c05da6c9602eecdb1b1f7c2f2d5b237b1752f
yiyo338 <32116409+yiyo338@users.noreply.github.com>
2019-05-28 10:36:47 -0600

    Estructuración del pl
    Se dividió el pl en secciones.

--- Logica.pl
+++ Logica.pl
@@ -1,73 +1,12 @@
 :-style_check(-singleton).
+:-dynamic
+	rpath/2.
 iniciar():-pregunta1().
 
-
-
-o(o(SN,SV)) --> sn(SN,_Gen,Num),sv(SV,Num);sn(SN,_Gen,Num); sv(SV, Num); yn(YN), sn(SN,_Gen,Num).
-
-
-sn(sn(DET,N),Gen,Num) --> det(DET,Gen,Num), n(N,Gen,Num); sal(SAL,Num),n(N,Gen,Num); n(N,Gen, Num).
-
-
-sv(sv(VT,SN),Num) --> vt(VT,Num), sn(SN,_Gen,_Num).
-sv(sv(VI),Num) --> vi(VI,Num).
-
-
-det(det(X),f,sg) --> [X], {member(X,[la,una,para])}.
-det(det(X),f,pl) --> [X], {member(X,[las,unas])}.
-det(det(X),m,sg) --> [X], {member(X,[el,un,en,para])}.
-det(det(X),m,pl) --> [X], {member(X,[los,unos])}.
-
-sal(sal(X), sg) --> [X] ,{member(X,[hola,buenosdias])}.
-
-yn(yn(X)) --> [X] ,{member(X,[si,no])}.
-
-vi(vi(ladra),sg) --> [ladra].
-vi(vi(ladran),pl) --> [ladran].
-vi(vi(ladran),sg) --> [estoy].
-vt(vt(estoy),sg) --> [estoy].
-vt(vt(voy),sg) --> [voy].
-vt(vt(muerden),pl) --> [muerden].
-
-
-n(n(perra),f,sg) --> [perra].
-n(n(perras),f,pl) --> [perras].
-n(n(X),m,sg) --> [DrWaze].
-n(n(X),m,sg) --> [X], {lugares(X)}.
-
-n(n(X),m,pl) --> [X], {member(X,[perros,huesos])}.
-
-%...DEFINICION DE LOS LUGARES DEL GRAFO...
-lugares(cartago).
-lugares(tresrios).
-lugares(sanjose).
-lugares(corralillo).
-lugares(musgoverde).
-lugares(paraiso).
-lugares(pacayas).
-lugares(cervantes).
-lugares(cachi).
-lugares(orosi).
-lugares(juanvinas).
-lugares(turrialba).
-
-
-%FUNCIONES PARA EL MANEJO DE LISTAS
-
-concatenar([],L,L).
-concatenar([C|R],L,[C|R1]):-  concatenar(R,L,R1).
-
-agregarini(X, L, [X|L]).
-
-eliminarini(L,L1):-
-agregarini(_,L1,L).
-
-juntar([C|R],L,F):- eliminarini(L, L1), concatenar([C|R],L1,F).
-
-sumar(X, Y, Z):-Z is X + Y.
-
-agregar(X, [], [X]).
-agregar(X, [C|R], [C|R1]):-agregar(X, R, R1).
+:-[arcos].
+:-[lugares].
+:-[gramatica].
+:-[manejo_listas].
 
 
 %...SERIE DE PREGUNTAS Y RESPUESTAS...
@@ -104,46 +43,6 @@ imprimir(O,[C|_],R,T):-ruta(O,C,R1,T1), sumar(T,T1,T2),juntar(R,R1,R2), writef('
 imprimir(O,[C|_],[],0):-ruta(O,C,R2,T2), writef('su ruta es %w y recorrera: %w km \n',[R2, T2]).
 
 
-
-miembro(X,[X|_]).
-miembro(X,[_|R]):-miembro(X,R).
-
-:-dynamic
-	rpath/2.
-
-arco(sanjose, cartago, 20).
-arco(sanjose,corralillo,22).
-arco(cartago,paraiso,10).
-arco(cartago,pacayas,13).
-arco(cartago,sanjose,20).
-arco(cartago,musgoverde,10).
-arco(cartago,tresrios,8).
-arco(corralillo, sanjose,22).
-arco(corralillo, musgoverde,10).
-arco(paraiso, cervantes,4).
-arco(paraiso, cachi,10).
-arco(paraiso,orosi,8).
-arco(pacayas, tresrios,15).
-arco(pacayas, cartago,13).
-arco(pacayas,cervantes,8).
-arco(musgoverde, corralillo,6).
-arco(musgoverde, cartago,10).
-arco(tresrios, sanjose,8).
-arco(tresrios, pacayas,15).
-arco(cervantes, juanvinas,5).
-arco(cervantes, pacayas,8).
-arco(cervantes, cachi,7).
-arco(cachi,orosi,12).
-arco(cachi, paraiso,10).
-arco(cachi,turrialba,40).
-arco(cachi,cervantes,7).
-arco(orosi, paraiso,8).
-arco(orosi,cachi,12).
-arco(juanvinas, turrialba,4).
-arco(turrialba, pacayas,18).
-arco(paraiso, cervantes,4).
-arco(paraiso, cachi,10).
-arco(paraiso, orosi,8).
 
 camino(Ini,Fin,Dist) :- arco(Ini,Fin,Dist).
 
